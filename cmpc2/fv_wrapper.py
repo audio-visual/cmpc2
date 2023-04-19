e532730d9806c9e62bc306294163d6e0e5f8b43c
wuyang <chenwuyangcn@gmail.com>
2023-04-19 14:59:28 +0800

    update

--- cmpc2/fv_wrapper.py
+++ cmpc2/fv_wrapper.py
@@ -16,6 +16,42 @@ class Head(nn.Module):
     def forward(self, x):
         return self.projection(x)
 
+def load_weights_from_cmpc(model, pretrained_dict):# load single model
+   
+    model_dict = model.state_dict()
+    # print('keys in init model dict:')
+    # for k,v in model_dict.items():
+    #     print(k)
+    # print('voice_subnet.bn1.bias',model.voice_subnet.bn1.bias)
+    # print()
+    # pretrained_dict = torch.load(path, map_location={'cuda:0': 'cpu'})
+    # pretrained_dict.map_location{} TODO 
+    # 1. filter out unnecessary keys
+    pretrained_dict = {k: v for k, v in pretrained_dict['model'].items() if k in model_dict}
+    # 2. overwrite entries in the existing state dict
+    # print('keys in filtered pretrained dict:')
+    # for k,v in pretrained_dict.items():
+    #     print(k)
+    # print('voice_subnet.bn1.bias',pretrained_dict['voice_subnet.bn1.bias'])
+    # print()
+    model_dict.update(pretrained_dict) 
+    # 3. load the new state dict
+    model.load_state_dict(model_dict)
+    print('loaded cmpc ckp ')
+    # return model # 可以不返回，直接修改了model里面的weight
+
+def load_model(model, checkpoint_fn): # load face-voice model
+    # model = models.__dict__[cfg['model']['arch']](**cfg['model']['args'])
+    print("loads chechpoint for both model:",checkpoint_fn)
+    ckp = torch.load(checkpoint_fn, map_location='cpu')
+    # model.load_state_dict({k.replace('module.', ''): ckp['model'][k] for k in ckp['model']})
+    model.load_state_dict(ckp['model'])
+    # logger.info('Load from {} at iteration #{}'.format(checkpoint_fn, ckp['epoch']))
+    model = model.cuda()
+    model.eval()
+
+    return model
+
 
 class FV_Wrapper(nn.Module):
     def __init__(self, pretrain, last_dim, proj_dim, proj, test_only=False):
@@ -30,8 +66,8 @@ class FV_Wrapper(nn.Module):
         # self.voice_subnet_proj = nn.Sequential(self.voice_subnet, self.projnet)
         # self.face_subnet_proj = nn.Sequential(self.face_subnet, self.projnet)
         # self.test_only = test_only
-    def get_models():
-        return self.voice_subnet, self.face_subnet
+    # def get_models(self,path):
+    #     return self.voice_subnet, self.face_subnet
 
     def forward(self, audio, frame):
         audio_emb = self.voice_subnet(audio)
--- cmpc2/model.py
+++ cmpc2/model.py
@@ -1,10 +1,12 @@
 import numpy as np 
 import torch
-from .fv_wrapper import  FV_Wrapper
+from .fv_wrapper import  FV_Wrapper,load_model
 
 
 
 def load(path):
     # load ckpt
+    # model load chechpoints
     fv = FV_Wrapper(pretrain=False, last_dim=512, proj_dim=512, proj=False, test_only=False)
-    return fv.get_models()
+    model = load_model(fv, path)
+    return model
